fa0f7ff0e03ae885c40c2caf9506682d38966729
Simon Tarberg <simontar@ug.kth.se>
2026-09-25 18:12:21 +0200

    Binär sökfunktion, fungerar i kattis

--- Bin_search.py
+++ Bin_search.py
@@ -1,1 +1,28 @@
-print("hej")
+def binary_search(the_list, key):
+    low = 0
+    high = len(the_list) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if the_list[mid] == key:
+            return key
+        elif the_list[mid] < key:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return None
+
+
+def main():
+    indata = input().strip()
+    the_list = indata.split()
+
+    key = input().strip()
+    while key != "#":
+        print(binary_search(the_list, key))
+        key = input().strip()
+
+
+main()
